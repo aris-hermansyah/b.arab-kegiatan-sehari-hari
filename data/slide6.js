f841172e6e07e0cdd3d7ee1d7b522cc057f21410
aris-hermansyah <88818089+aris-hermansyah@users.noreply.github.com>
2021-08-12 13:56:20 +0800

    Add files via upload

---
+++ data/slide6.js
@@ -0,0 +1,5 @@
+(function(){var loadHandler=window['sl_{6A275CB6-E627-4AA0-B081-88B4948E4D29}'];loadHandler&&loadHandler(5, '<div id="spr0_5f78307"><div id="spr1_5f78307" class="kern slide"><img id="img2_5f78307" src="data/img32.jpg" width="720px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_5f78307" style="left:642px;"><img id="img0_5f78307" src="data/img10.png" width="78" height="540" alt=""/></div></div><div id="spr2_5f78307" class="kern slide"><div id="spr4_5f78307" style="left:80px;top:263px;"><img id="img1_5f78307" src="data/img31.png" width="522" height="49" alt="\
+\
+والسلام عليكم و رحمة الله و بركاته \
+\
+"/></div></div></div>', '{"s":[]}');})();
